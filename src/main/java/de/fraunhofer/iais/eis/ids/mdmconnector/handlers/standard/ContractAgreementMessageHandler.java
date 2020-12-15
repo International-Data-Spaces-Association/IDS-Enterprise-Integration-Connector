cd49a73d7efc66d2134d6988737e5aafb8d5dd27
dkubitza <dennis.oliver.kubitza@fraunhofer.iais.de>
2020-12-15 09:42:39 +0100

    Buildable from Open Source Dependencies

--- src/main/java/de/fraunhofer/iais/eis/ids/mdmconnector/handlers/standard/ContractAgreementMessageHandler.java
+++ src/main/java/de/fraunhofer/iais/eis/ids/mdmconnector/handlers/standard/ContractAgreementMessageHandler.java
@@ -4,10 +4,10 @@ import de.fraunhofer.iais.eis.*;
 import de.fraunhofer.iais.eis.ids.component.core.MessageAndPayload;
 import de.fraunhofer.iais.eis.ids.component.core.MessageHandler;
 import de.fraunhofer.iais.eis.ids.component.core.RejectMessageException;
+import de.fraunhofer.iais.eis.ids.component.core.map.DefaultSuccessMAP;
 import de.fraunhofer.iais.eis.ids.connector.commons.contract.map.ContractRejectionMAP;
 import de.fraunhofer.iais.eis.ids.mdmconnector.artifact.ArtifactIndex;
 import de.fraunhofer.iais.eis.ids.mdmconnector.components.ContractUtil;
-import de.fraunhofer.iais.eis.ids.mdmconnector.components.DefaultSuccessMAP;
 import de.fraunhofer.iais.eis.ids.component.core.util.CalendarUtil;
 import de.fraunhofer.iais.eis.ids.component.ecosystemintegration.daps.DapsSecurityTokenProvider;
 import de.fraunhofer.iais.eis.ids.connector.commons.contract.map.ContractAgreementMAP;
@@ -75,19 +75,8 @@ public class ContractAgreementMessageHandler implements MessageHandler<ContractA
 			ContractAgreement agreementContract = (ContractAgreement) contractAgreementMAP.getPayload().get();
 			logger.info("...passed:");
 				if (contractUtil.compareWithIndex(agreementContract, message.getSenderAgent())) {
-					MessageProcessedNotificationMessage confirm = new MessageProcessedNotificationMessageBuilder()
-							._issuerConnector_(infrastructureComponent.getId())
-							._issued_(CalendarUtil.now())
-							._senderAgent_(infrastructureComponent.getMaintainer())
-							._securityToken_(daps.getSecurityTokenAsDAT())
-							._modelVersion_(infrastructureComponent.getOutboundModelVersion())
-							._correlationMessage_(message.getId())
-							._recipientConnector_(Util.asList(message.getIssuerConnector()))
-							._recipientAgent_(Util.asList(message.getSenderAgent()))
-							.build();
-
 					MessageAndPayload successMessage =
-							new DefaultSuccessMAP(confirm);
+							new DefaultSuccessMAP(infrastructureComponent.getId(), infrastructureComponent.getOutboundModelVersion(), message.getId(), daps.getSecurityTokenAsDAT(), infrastructureComponent.getMaintainer());
 
 					URI targetUri = agreementContract.getPermission().get(0).getTarget();
 					Artifact targetArtifact = artifactIndex.getArtifactAsObject(targetUri).get();
